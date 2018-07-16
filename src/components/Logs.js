26e4e279eb766b47a928ca69c3dd0078d93d9e69
Rafael Akiyoshi <rafah94@hotmail.com>
2018-07-16 17:25:12 -0300

    creating Logs component

---
+++ src/components/Logs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import './Logs.css';
+  export default class Logs extends Component {
+    render() {
+      return (
+        <div className="Logs">
+          <ListGroup>
+            {this.props.logs.map((log, index) => {
+              return (
+                <ListGroupItem key={index}>{log}</ListGroupItem>
+              )
+            })}
+          </ListGroup>
+        </div>
+    );
+  }
+}
