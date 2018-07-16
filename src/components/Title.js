fad97f72792390a8e3405e3decda3c8c800a1fea
Rafael Akiyoshi <rafah94@hotmail.com>
2018-07-16 19:30:38 -0300

    fixing Title Component

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export default class Title extends Component {
   render() {
     return (
         <div className="Title-header">
-          <h1 className="Title-title">{this.props.name}</h1>
+          <p className="Title-title">{this.props.name}</p>
         </div>
     );
   }
